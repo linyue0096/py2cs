212a91d4d08580062555bc2199b8ff916350e917
ccckmit <ccckmit@gmail.com>
2025-09-18 14:15:31 +0800

    math to gfm

---
+++ 01-程式與數學/togfm.sh
@@ -0,0 +1,10 @@
+#cp -r ./$1 ./$1/_doc
+# set -x
+mkdir -p ./$1/_doc/
+cp ./$1/*.md ./$1/_doc/
+
+# perl -i.bak.markdown  -p \
+perl -0777 -i -p \
+  -e 's/\\\[\s*(.*?)\s*\\\]/\n```math\n\1\n```\n/gs;' \
+  -e 's/\\\(\s*(.*?)\s*\\\)/ \$`\1`\$ /g;' \
+  ./$1/_doc/*.md
